f87d12a03d6846b4dfc859e875fd4322d1f49a96
Tom Shaked <tomshkd1@gmail.com>
2022-08-25 08:42:49 +0300

    3rd commit

--- Score.py
+++ Score.py
@@ -5,24 +5,20 @@ import Utils
 # Each time the user is winning a game, the points he one will be added to his current amount of
 # point saved in a file.
 def add_score(difficulty):
-    points_of_winning = (int(difficulty) * 3) + 5
+    points_won = (int(difficulty) * 3) + 5
     f = open(Utils.SCORES_FILE_NAME, "a+")
-    f.write(str(points_of_winning) + ' ')
     f.seek(0)
-    r = f.readline()
-    if len(r[0]) > 1:
-        r2=r[0][-1]
-        total = int(r[0][-1])
-        for num in r[0]:
-            total += int(points_of_winning)
-        f.truncate(0)
-        f.write(str(total))
-        f.close()
-    else:
-        f.close()
-
-
-
+    data = f.readlines()
+    if len(data) >= 1:
+        list_of_points = [data[0]]
+        f.seek(0)
+        f.truncate()
+        accumulated_score = int(list_of_points[0])
+        new_score = points_won + accumulated_score
+        f.write(str(new_score))
+    elif len(data) == 0:
+        f.seek(0)
+        f.write(str(points_won))
 
 
 print(add_score(1))
