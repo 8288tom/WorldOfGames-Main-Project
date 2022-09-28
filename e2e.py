6950f1c21e2a6760e7de04272336409998b73d6d
Tom Shaked <tomshkd1@gmail.com>
2022-09-28 18:04:50 +0300

    e2e.py

---
+++ e2e.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
+def test_scores_service(url):
+    my_drive = webdriver.Chrome("C:/Users/tom.shaked/Downloads/chromedriver.exe")
+    my_drive.get(url)
+    WebDriverWait(my_drive, 15).until(
+        EC.element_to_be_clickable(
+            (By.ID, "score")))
+    score = my_drive.find_element(By.ID, "score").text
+    sleep(3)
+    try:
+        if 0 < int(score) < 1001:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+def main_function():
+    result = test_scores_service("http://127.0.0.1:5001")
+    if result is True:
+        return 0
+    if result is False:
+        return -1
+
+print(main_function())
